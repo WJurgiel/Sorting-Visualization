5a4253be01deb6368f0fe8f556e474aeed688041
WJurgiel <wjurgiel@student.agh.edu.pl>
2025-01-23 20:25:12 +0100

    Column width is calculated dynamically

--- CONFIG.h
+++ CONFIG.h
@@ -4,11 +4,10 @@
 
 #ifndef CONFIG_H
 #define CONFIG_H
-
-constexpr float ENTITY_WIDTH = 20;
-constexpr float HORIZONTAL_OFFSET = 10;
-constexpr float WINDOW_WIDTH = 800;
-constexpr float WINDOW_HEIGHT = 600;
-constexpr int NUM_ENTITIES = 10;
+constexpr float HORIZONTAL_OFFSET = 1;
+constexpr float WINDOW_WIDTH = 1600;
+constexpr float WINDOW_HEIGHT = 900;
+constexpr int NUM_ENTITIES = 500;
+constexpr float ENTITY_WIDTH = WINDOW_WIDTH / NUM_ENTITIES - HORIZONTAL_OFFSET;
 
 #endif //CONFIG_H
