e8f0ff802da51371489ba73bbad3e4adbafdd582
WJurgiel <wjurgiel@student.agh.edu.pl>
2025-01-25 13:49:43 +0100

    Columns are being sorted (no simulation steps yet)

--- CONFIG.h
+++ CONFIG.h
@@ -5,9 +5,10 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 constexpr float HORIZONTAL_OFFSET = 1;
+constexpr float VERTICAL_OFFSET = 50;
 constexpr float WINDOW_WIDTH = 1600;
 constexpr float WINDOW_HEIGHT = 900;
-constexpr int NUM_ENTITIES = 500;
+constexpr int NUM_ENTITIES = 400;
 constexpr float ENTITY_WIDTH = WINDOW_WIDTH / NUM_ENTITIES - HORIZONTAL_OFFSET;
 
 #endif //CONFIG_H
--- Entity.cpp
+++ Entity.cpp
@@ -10,11 +10,15 @@ void Entity::draw(sf::RenderTarget &target, sf::RenderStates states) const {
     target.draw(column,states);
 }
 
-Entity::Entity(float height, float posX){
+Entity::Entity(int id, float height, float posX){
+    this->id = id;
     this->entityHeight = height;
     this->posX = posX;
     this->posY = WINDOW_HEIGHT - this->entityHeight;
     this->column.setSize({this->chunkWidth, entityHeight});
     this->column.setPosition(posX, this->posY);
     this->column.setFillColor(sf::Color::Cyan);
+}
+float & Entity::getEntityHeight() {
+    return this->entityHeight;
 }
