82d4aa856606d614d341ed3755bf078fd885177f
WJurgiel <wjurgiel@student.agh.edu.pl>
2025-03-25 21:23:54 +0100

    test: added one dummy test file (will add test here later)

---
+++ SortingProgram/Test/SortTest.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Wojtek on 24.03.2025.
+//
+
+#include <EntityManager.h>
+#include <numeric>
+#include <SortFactory.h>
+#include <SortVisualizer.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+
+
+TEST(SortingCorrectness, BubbleSort ) {
+    ASSERT_EQ(5,5);
+}
