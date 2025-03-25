66fed85ccc986a84bfadbe2bb6c043a8a6a3a156
WJurgiel <wjurgiel@student.agh.edu.pl>
2025-03-25 21:46:14 +0100

    test: test cases now work when run all at time

--- SortingProgram/Test/AppTest.cpp
+++ SortingProgram/Test/AppTest.cpp
@@ -8,43 +8,27 @@
 
 #include "AlgorithmType.h"
 TEST(AppEssentials, IsInitializing) {
-    char** argv;
+    char* argv[] = { nullptr, nullptr };
     std::vector<Entity> testEntities;
     App* app = App::getInstance(&testEntities, argv);
     ASSERT_NE(app, nullptr);
 
-    delete app;
+    App::destroyInstance();
 }
 TEST(AppEssentials, CorrectArgumentPassed) {
-    char** argv = new char*[2];
-    argv[0] = new char[3]{'a', 'p', 'p'};
-
-    argv[1] = new char[2]{'1', '\0'};
+    char* argv[] = { (char*)"app", (char*)"1" };
     std::vector<Entity> testEntities;
     App* app = App::getInstance(&testEntities, argv);
-    ASSERT_EQ(BUBBLE_SORT, app->getAlgorithmType());
+    ASSERT_EQ(INSERTION_SORT, app->getAlgorithmType());
 
-    delete[] argv;
-    delete app;
+    App::destroyInstance();
 }
 TEST(AppEssentials, CorrectSortName) {
-    char** argv = new char*[2];
-    argv[0] = new char[3]{'a', 'p', 'p'};
-    argv[1] = new char[2]{'0', '\0'};
-    char** argv2 = new char*[2];
-    argv2[0] = new char[3]{'a', 'p', 'p'};
-    argv2[1] = new char[2]{'1', '\0'};
+    char* argv[] = { (char*)"app", (char*)"0" };
     std::vector<Entity> testEntities;
     App* app = App::getInstance(&testEntities, argv);
     TextLog algorithmNameTextLog = app->getAlgorithmNameTextLog();
     ASSERT_EQ(algorithmNameTextLog.getMessage(), "alg_name: Bubble Sort");
 
     App::destroyInstance();
-    App* app2 = App::getInstance(&testEntities, argv2);
-    algorithmNameTextLog = app->getAlgorithmNameTextLog();
-    ASSERT_EQ(algorithmNameTextLog.getMessage(), "alg_name: Insertion Sort");
-
-    delete[] argv;
-    delete[] argv2;
-    delete app;
 }
